a2aae41ae617b21be493c1c534c477fa8746359f
Palente <palentegaming@gmail.com>
2022-07-11 00:32:15 +0200

    Init plugin

---
+++ Managers/EventsManager.cs
@@ -0,0 +1,22 @@
+﻿using OpenAPI.Events;
+using OpenAPI.Events.Player;
+
+namespace OneBlock.Managers
+{
+    internal class EventsManager : IEventHandler
+    {
+        private readonly Plugin _caller;
+        public EventsManager(Plugin caller)
+        {
+            _caller = caller;
+        }
+
+
+        [EventHandler]
+        public void OnJoin(PlayerJoinEvent ev)
+        {
+            
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using OneBlock.Managers;
+
+using OpenAPI;
+using OpenAPI.Plugins;
+
+namespace OneBlock
+{
+    [OpenPluginInfo(Name = "OneBlock",Description = "You dreamed about it, here it is", Author = "Palente", Version = "", Website = "https://github.com/Palente")]
+    public class Plugin : OpenPlugin
+    {
+        public OpenApi API;
+        public override void Disabled(OpenApi api)
+        {
+            //Destroy game?
+        }
+
+        public override void Enabled(OpenApi api)
+        {
+            API = api;
+            api.EventDispatcher.RegisterEvents(new EventsManager(this));
+        }
+    }
+}
